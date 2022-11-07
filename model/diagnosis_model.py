8b91dbb61785b98ac58a634744ec000369ac0c9d
Cristo Jesus Carrillo Sanchez <cristocarrillo@wposs.com>
2022-11-07 13:56:58 -0500

    modificacion conexion

--- model/diagnosis_model.py
+++ model/diagnosis_model.py
@@ -4,7 +4,6 @@ def insert_sintomas(sintomas):
     try:
         collect = gha.diagnosis
         collect.insert_one(sintomas)
-        close_sesion()
         print("registro agregado")
     except Exception as e:
         print(f"error {e}")
