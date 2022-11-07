c0c2ec4440464c0a96e8a2d721e7e6abad5a50fe
Cristo Jesus Carrillo Sanchez <cristocarrillo@wposs.com>
2022-11-07 12:34:17 -0500

    configuracion entorno

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 app.register_blueprint(diagnosis)
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port="80",debug=True)
+# if __name__=="__main__":
+#     app.run(host="0.0.0.0",port="80",debug=True)
