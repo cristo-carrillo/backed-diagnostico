bf92c72b986905f29a3992b849dce1c53d7f2d38
Cristo Jesus Carrillo Sanchez <cjcarrillos@ufpso.edu.co>
2022-11-24 00:01:56 -0500

    ➕ ADD: add atribute exist to insert kin

--- controller/kin_controller.py
+++ controller/kin_controller.py
@@ -5,14 +5,14 @@ def insert_kin_family(dict_kin):
     kin_one = format_kin(dict_kin)
     parent = kin_one.family_exist()
     if len(parent)>0:
-        return {"msg":"El familiar ya estaba asociado"}
+        return {"msg":"El familiar ya estaba asociado", "exists":True}
     msg = send_email(dict_kin['user_second'], dict_kin['name_main'])
     if msg['cod'] == '00':
         dict_kin_two = determinate_kin(dict_kin)
         kin_two = format_kin(dict_kin_two)
         kin_two.insert_kin_many(kin_one)
-        return {'msg':'Familiar agregado correctamente'}
-    return {'msg':'Ups algo salio mal'}
+        return {'msg':'Familiar agregado correctamente',"exists":False}
+    return {'msg':'Ups algo salio mal', "exists":False}
 
 def format_kin(dict_kin):
     
